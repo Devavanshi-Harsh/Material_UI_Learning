1c1b527360e76272c5ffc81e2c9cc23ac60027c6
Harsh Devavanshi <hdevavan@gmail.com>
2024-01-12 20:33:45 +0530

    Typography will show based on a condition

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css';
-import { MuiTypography } from './components/MuiTypography';
+import { useState } from 'react'
+import './App.css'
+import { MuiTypography } from './components/MuiTypography'
 
 function App() {
+  const [showTypography, setshowTypography] = useState(false)
+
   return (
-    <div className="App">
-      <MuiTypography />
+    <div className='App'>
+      <button onClick={() => setshowTypography(!showTypography)}>
+        Typography
+      </button>
+      {showTypography ? <MuiTypography /> : null}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
